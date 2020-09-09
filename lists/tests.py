6e99f019f626add81267e33571dfa9e54855a786
unknown <wirifrits@gmail.com>
2020-09-09 00:25:35 -0300

    Persistindo os Dados

--- lists/tests.py
+++ lists/tests.py
@@ -1,14 +1,35 @@
 from django.test import TestCase
+from lists.models import Item
 
 class HomePageTest(TestCase):
 
     def test_uses_home_template(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
-        self.assertTrue(
+        """self.assertTrue(
         any(row.text == '1: Estudar testes funcionais' for row in rows),
-        "New to-do item did not appear in table")
+        "New to-do item did not appear in table")"""
+        self.assertIn('1: Estudar testes funcionais', [row.text for row in rows])
 def test_can_save_a_POST_request(self):
    response = self.client.post('/', data={'item_text': 'A new list item'})
    self.assertIn('A new list item', response.content.decode())
    self.assertTemplateUsed(response, 'home.html')
+
+class ItemModelTest(TestCase):
+
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = 'O primeiro item'
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'O segundo item'
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, 'O primeiro item')
+        self.assertEqual(second_saved_item.text, 'O segundo item')   
