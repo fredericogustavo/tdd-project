3eff7b0d3722a7dc876b1a7cb0a15bdc8a0d8fcf
unknown <wirifrits@gmail.com>
2020-09-08 22:48:52 -0300

    Utilizando o Django Test Client

--- lists/tests.py
+++ lists/tests.py
@@ -1,4 +1,4 @@
-from django.urls import resolve
+"""from django.urls import resolve
 from django.test import TestCase
 from django.http import HttpRequest
 
@@ -10,10 +10,19 @@ class HomePageTest(TestCase):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
 
-    def test_home_page_returns_correct_html(self):
-        request = HttpRequest()
-        response = home_page(request)
-        html = response.content.decode('utf8')
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>To-Do lists</title>', html)
-        self.assertTrue(html.endswith('</html>'))
+def test_home_page_returns_correct_html(self):
+    response = self.client.get('/')  
+
+    html = response.content.decode('utf8')  
+    self.assertTrue(html.startswith('<html>'))
+    self.assertIn('<title>To-Do lists</title>', html)
+    self.assertTrue(html.strip().endswith('</html>'))
+
+    self.assertTemplateUsed(response, 'home.html')""" 
+from django.test import TestCase
+
+class HomePageTest(TestCase):
+
+    def test_uses_home_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
