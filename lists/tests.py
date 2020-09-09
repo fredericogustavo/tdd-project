461193f90bbd906f2074ccc38c020005551fb3ab
unknown <wirifrits@gmail.com>
2020-09-08 23:40:07 -0300

    Processando Requisição POST

--- lists/tests.py
+++ lists/tests.py
@@ -1,24 +1,3 @@
-"""from django.urls import resolve
-from django.test import TestCase
-from django.http import HttpRequest
-
-from lists.views import home_page
-
-class HomePageTest(TestCase):
-
-    def test_root_url_resolves_to_home_page_view(self):
-        found = resolve('/')
-        self.assertEqual(found.func, home_page)
-
-def test_home_page_returns_correct_html(self):
-    response = self.client.get('/')  
-
-    html = response.content.decode('utf8')  
-    self.assertTrue(html.startswith('<html>'))
-    self.assertIn('<title>To-Do lists</title>', html)
-    self.assertTrue(html.strip().endswith('</html>'))
-
-    self.assertTemplateUsed(response, 'home.html')""" 
 from django.test import TestCase
 
 class HomePageTest(TestCase):
@@ -26,3 +5,9 @@ class HomePageTest(TestCase):
     def test_uses_home_template(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
+        self.assertTrue(
+        any(row.text == '1: Estudar testes funcionais' for row in rows),
+        "New to-do item did not appear in table")
+def test_can_save_a_POST_request(self):
+    response = self.client.post('/', data={'item_text': 'A new list item'})
+    self.assertIn('A new list item', response.content.decode())
