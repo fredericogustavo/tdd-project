e26d940bbd58ad3b6b752c8cd6f6cc95e61bbb54
unknown <wirifrits@gmail.com>
2020-10-18 15:37:39 -0300

    De volta ao teste funcional - PARTE 1

--- lists/views.py
+++ lists/views.py
@@ -2,30 +2,22 @@ from django.shortcuts import redirect, render
 from lists.models import Item, List
 from django.core.exceptions import ValidationError
 
-"""def home_page(request):
-    if request.method == 'POST':
-        new_item_text = request.POST['item_text']
-        Item.objects.create(text=new_item_text)
-        return redirect('/lists/the-only-list-in-the-world/')
-
-    return render(request, 'home.html')"""
 	
 def home_page(request):
     return render(request, 'home.html')	
 
 def view_list(request, list_id):
-    my_list = List.objects.get(id=list_id)
-    return render(request, 'list.html', {'list': my_list})
+    list_ = List.objects.get(id=list_id)
+    if request.method == 'POST':
+        Item.objects.create(text=request.POST['item_text'], list=list_)
+        return redirect(f'/lists/{list_.id}/')
+    return render(request, 'list.html', {'list': list_})
 	
-"""def new_list(request):
-    my_list = List.objects.create()
-    Item.objects.create(text=request.POST['item_text'], list=my_list)
-    return redirect(f'/lists/{my_list.id}/')"""
 	
-def add_item(request, list_id):
+"""def add_item(request, list_id):
     list_ = List.objects.get(id=list_id)
     Item.objects.create(text=request.POST['item_text'], list=list_)
-    return redirect(f'/lists/{list_.id}/')
+    return redirect(f'/lists/{list_.id}/')"""
 
 def new_list(request):
     list_ = List.objects.create()
