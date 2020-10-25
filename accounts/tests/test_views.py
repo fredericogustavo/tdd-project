e8941b10ce6e89c17076cb3f38961690e547a9f1
unknown <wirifrits@gmail.com>
2020-10-25 19:59:53 -0300

    Testing the Django Messages Framework

--- accounts/tests/test_views.py
+++ accounts/tests/test_views.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 import accounts.views
-from unittest.mock import patch
+from unittest.mock import patch, call
 
 class SendLoginEmailViewTest(TestCase):
 
@@ -40,4 +40,24 @@ class SendLoginEmailViewTest(TestCase):
         self.assertTrue(self.send_mail_called)
         self.assertEqual(self.subject, 'Your login link for Superlists')
         self.assertEqual(self.from_email, 'noreply@superlists')
-        self.assertEqual(self.to_list, ['edith@example.com'])	
+        self.assertEqual(self.to_list, ['edith@example.com']) 
+  
+    def test_adds_success_message(self):
+        response = self.client.post('/accounts/send_login_email', data={
+            'email': 'edith@example.com'
+        }, follow=True)
+        message = list(response.context['messages'])[0]
+        self.assertEqual(message.message,"Check your email, we've sent you a link you can use to log in.")
+        self.assertEqual(message.tags, "success")
+  
+    @patch('accounts.views.messages')
+    def test_adds_success_message_with_mocks(self, mock_messages):
+        response = self.client.post('/accounts/send_login_email', data={
+            'email': 'edith@example.com'
+        })
+
+        expected = "Check your email, we've sent you a link you can use to log in."
+        self.assertEqual(
+            mock_messages.success.call_args,
+            call(response.wsgi_request, expected),
+        )  
